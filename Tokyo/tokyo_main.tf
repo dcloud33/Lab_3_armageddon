050c036ae3d47289bf78dbdcbb091c798dbe64b4
Davey Wheeling <daveywheeling426@gmail.com>
2026-03-26 15:13:43 -0400

    updated Tokyo and Sao Paulo main

--- Tokyo/tokyo_main.tf
+++ Tokyo/tokyo_main.tf
@@ -5,7 +5,7 @@ locals {
 }
 
 module "network" {
-  source               = "../modules/network"
+  source               = "modules/network"
   name_prefix          = local.name_prefix
   vpc_cidr             = var.vpc_cidr
   public_subnet_cidrs  = var.public_subnet_cidrs
@@ -17,7 +17,7 @@ module "network" {
 }
 
 module "compute" {
-  source = "../modules/compute"
+  source = "modules/compute"
 
   name_prefix        = local.name_prefix
   vpc_id             = module.network.vpc_id
@@ -45,7 +45,7 @@ module "compute" {
 
 
 module "data" {
-  source = "./modules/data"
+  source = "modules/data"
 
   name_prefix         = local.name_prefix
   vpc_id              = module.network.vpc_id
